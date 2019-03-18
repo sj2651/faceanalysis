13413431ab27c7c0763895bdac5f42d30c760418
soldesk <soldesk@soldesk-PC>
2019-03-18 14:44:36 +0900

    star

---
+++ faceanalysis/src/main/java/kr/co/faceanalysis/persistence/DAO.java
@@ -0,0 +1,11 @@
+package kr.co.faceanalysis.persistence;
+
+import java.util.List;
+
+import kr.co.faceanalysis.dto.MemberDTO;
+
+public interface DAO {
+	public List<MemberDTO> member_selectAll();
+	
+
+}
