b6797b6b4d2c015da1030268b86f3fe8811a71de
soldesk <soldesk@soldesk-PC>
2019-03-22 18:06:00 +0900

    product 수정

--- src/main/java/kr/co/faceanalysis/persistence/Product_impl.java
+++ src/main/java/kr/co/faceanalysis/persistence/Product_impl.java
@@ -4,12 +4,21 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import kr.co.faceanalysis.vo.Option;
 import kr.co.faceanalysis.vo.ProductVO;
 
-public interface Product_impl  {
+public interface Product_impl {
 	List<ProductVO> findAllProduct();
+
 	List<ProductVO> findByBrand(String p_brand);
+
 	List<ProductVO> findByCate(String p_cate);
+
 	List<ProductVO> findByName(String p_name);
+
 	ProductVO findById(int p_no);
+
+	Option[] searchBy = { new Option(0, "검색 없음"), new Option(1, "브랜드명"), new Option(2, "카테고리명")};
+	Option[] orderBy = { new Option(0, "최근 글"), new Option(1, "오래된 글"), new Option(2, "글쓴이") };
+
 }
